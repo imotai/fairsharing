878c75b3fa82155e3a13ccea52c541512f45458d
KieSun <zx597813039@gmail.com>
2023-04-18 15:27:04 +0800

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
-import Head from 'next/head'
 import Layout from '@/layout'
+import { Typography, Button } from '@mui/material'
 
 export default function Home() {
-  return <Layout>11</Layout>
+  return (
+    <Layout>
+      <div className="flex items-center justify-center h-full flex-col">
+        <Typography variant="h2" className="font-bold text-[#272D37] mb-6">
+          A tool for LaborDAO
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          className="text-lg text-[#5F6D7E] mb-12"
+        >
+          Upgrade your side project into a DAO and turbocharge it.
+        </Typography>
+        <Button size="large" variant="contained">
+          Create a project
+        </Button>
+      </div>
+    </Layout>
+  )
 }

f5f5282257690758b7ee6f5b2b42ad8aafdd3a7e
bobochained <bobchained@gmail.com>
2023-04-29 15:34:27 +1200

    contract: deposit

--- contracts/contracts/FairSharing.sol
+++ contracts/contracts/FairSharing.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 // Uncomment this line to use console.log
@@ -11,22 +11,26 @@ contract FairSharing is ERC20, Ownable {
     using ECDSA for bytes32;
 
     // Mapping to store the membership status of an address. true: active, false: inactive
-    mapping (address => bool) public members;
+    mapping(address => bool) public members;
     // Array to store the list of member addresses. Contain both active and inactive members
     address[] public membersList;
-    uint totalMembers; 
+    uint totalMembers;
 
-    mapping (uint => bool) public claimed;
+    mapping(uint => bool) public claimed;
 
     struct Vote {
         address voter;
         bool approve;
         bytes signature;
-    }    
-    
-    constructor(string memory name, string memory symbol, address[] memory _membersList) ERC20(name, symbol) {
+    }
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address[] memory _membersList
+    ) ERC20(name, symbol) {
         membersList = _membersList;
-        for (uint i=0; i<_membersList.length; i++) {
+        for (uint i = 0; i < _membersList.length; i++) {
             members[_membersList[i]] = true;
         }
     }
@@ -41,27 +45,54 @@ contract FairSharing is ERC20, Ownable {
         members[member] = false;
         totalMembers--;
     }
- 
-    function claim(uint contributionId, uint points, Vote[] calldata votes) external {
+
+    function claim(
+        uint contributionId,
+        uint points,
+        Vote[] calldata votes
+    ) external {
         require(!claimed[contributionId], "Already claimed");
         require(members[msg.sender], "Not a member");
         uint approvedVotes;
         // TODO: remove duplicated vote?
-        for (uint i=0; i<votes.length; i++) {
-            bytes memory data = abi.encodePacked(msg.sender, contributionId, votes[i].voter, votes[i].approve, points);
-            address dataSigner = keccak256(data).toEthSignedMessageHash().recover(votes[i].signature);
-            require(dataSigner == votes[i].voter, "Wrong signature"); 
+        for (uint i = 0; i < votes.length; i++) {
+            bytes memory data = abi.encodePacked(
+                msg.sender,
+                contributionId,
+                votes[i].voter,
+                votes[i].approve,
+                points
+            );
+            address dataSigner = keccak256(data)
+                .toEthSignedMessageHash()
+                .recover(votes[i].signature);
+            require(dataSigner == votes[i].voter, "Wrong signature");
             if (votes[i].approve) {
                 approvedVotes++;
             }
         }
-        require (approvedVotes >= totalMembers/2, "Not enough voters");
+        require(approvedVotes >= totalMembers / 2, "Not enough voters");
 
-        _mint(msg.sender, points); 
-        claimed[contributionId] = true; 
+        _mint(msg.sender, points);
+        claimed[contributionId] = true;
     }
 
     function deposit() external payable {
+        uint totalToken;
+        for (uint i = 0; i < membersList.length; i++) {
+            if (members[membersList[i]]) {
+                totalToken += balanceOf(membersList[i]);
+            }
+        }
+        uint valuePerToken = msg.value / totalToken;
 
+        for (uint i = 0; i < membersList.length; i++) {
+            if (members[membersList[i]]) {
+                (bool success, ) = membersList[i].call{
+                    value: valuePerToken * balanceOf(membersList[i])
+                }("");
+                require(success, "Transfer failed");
+            }
+        }
     }
 }
--- contracts/test/FairSharing.ts
+++ contracts/test/FairSharing.ts
@@ -1,8 +1,10 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { utils } from "ethers";
+import { utils, BigNumber } from "ethers";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { FairSharing } from "../typechain-types";
+
 
 describe("FairSharing", function () {
   async function deployFairSharingFixture() {
@@ -154,4 +156,72 @@ describe("FairSharing", function () {
       ).to.be.revertedWith("Already claimed");
     });
   });
+
+  describe("Deposit", function () {
+    const claim = async (fairSharing: FairSharing, contributionId: number, points: BigNumber, voters: SignerWithAddress[], contributor: SignerWithAddress) => {
+      // const contributionId = 1;
+      // const contributor = otherAccounts[0];
+      // const points = utils.parseEther("1");
+      const votes = await Promise.all(
+        voters.map(async (voterAccount) => {
+          const voter = await voterAccount.getAddress();
+          const approve = true;
+          const msgHash = utils.solidityKeccak256(
+            ["address", "uint256", "address", "bool", "uint256"],
+            [contributor.address, contributionId, voter, approve, points]
+          );
+          const signature = await voterAccount.signMessage(
+            utils.arrayify(msgHash)
+          );
+          return {
+            voter,
+            approve,
+            signature,
+          };
+        })
+      );
+
+      const claimer = contributor;
+
+      const tx = await fairSharing
+        .connect(claimer)
+        .claim(contributionId, points, votes);
+      await tx.wait();
+
+      const claimedToken = await fairSharing.balanceOf(claimer.address);
+      console.log('wtf claimedToken', claimer.address,  claimedToken);
+      
+      expect(claimedToken.eq(points)).to.be.true;
+    }
+
+    it("Should evenly distribute payment", async function () {
+      const { fairSharing, otherAccounts } = await loadFixture(
+        deployFairSharingFixture
+      );
+
+      const points1 = utils.parseEther("1");
+      const points2 = utils.parseEther("4");
+      const member1 = otherAccounts[0]
+      const member2 = otherAccounts[1]
+
+      const balanceBefore1 = await member1.getBalance()
+      const balanceBefore2 = await member2.getBalance()
+      console.log('balanceBefore1', balanceBefore1);
+      
+
+      console.log('member1 address', member1.address);
+
+      await claim(fairSharing, 1, points1, otherAccounts.slice(0, 2), member1)
+      await claim(fairSharing, 2, points2, otherAccounts.slice(0, 2), member2)
+
+      const tx = await fairSharing.deposit({value: utils.parseEther("9")})
+      await tx.wait()
+      const balanceAfter1 = await member1.getBalance()
+      const balanceAfter2 = await member2.getBalance()
+     
+      expect(balanceAfter1.sub(balanceBefore1).eq(utils.parseEther("3")))
+      expect(balanceAfter2.sub(balanceBefore2).eq(utils.parseEther("6")))
+    }); 
+  });
 });
+

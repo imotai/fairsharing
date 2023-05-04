8bf2861ad8a096111e960c19c19d306b2e8133b9
Bruce Xu <bruce.xu@lxdao.io>
2023-05-04 13:29:44 +1200

    Fix cicd issue

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,21 +1,27 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function main() {
-  const signers = await ethers.getSigners();
-  const members = [await signers[0].getAddress(), await signers[1].getAddress(), await signers[2].getAddress()]
-  const FairSharing = await ethers.getContractFactory("FairSharing");
-  const fairSharing = await FairSharing.deploy("TokenName", "TokenSymbol", members);
+  const signers = await ethers.getSigners()
+  const members = [
+    await signers[0].getAddress(),
+    await signers[1].getAddress(),
+    await signers[2].getAddress(),
+  ]
+  const FairSharing = await ethers.getContractFactory('FairSharing')
+  const fairSharing = await FairSharing.deploy(
+    'TokenName',
+    'TokenSymbol',
+    members
+  )
 
-  await fairSharing.deployed();
+  await fairSharing.deployed()
 
-  console.log(
-    `Deployed to ${fairSharing.address}`
-  );
+  console.log(`Deployed to ${fairSharing.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
